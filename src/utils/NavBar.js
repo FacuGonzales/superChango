cf7db9bf7cc241f60aece6c19dfd0c1ad30240d1
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-12-28 19:05:15 -0300

    Se arma navBar

--- src/utils/NavBar.js
+++ src/utils/NavBar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 
 import SavingsIcon from '@mui/icons-material/Savings';
@@ -8,144 +9,130 @@ import WineBarIcon from '@mui/icons-material/WineBar';
 import SanitizerIcon from '@mui/icons-material/Sanitizer';
 import HomeIcon from '@mui/icons-material/Home';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-import { Tooltip, Fab } from '@mui/material';
+import DehazeIcon from '@mui/icons-material/Dehaze';
+import CloseIcon from '@mui/icons-material/Close';
+
+import { Tooltip, Fab, Button } from '@mui/material';
 
 import logo from '../assets/logoHeader.png'
 
 import CardWidget from './CardWidget';
 
+
+
 const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+        console.log(isOpen)
+    }
+    
     return(
-        <>
         <nav className="navBar">
-            {/* <Link to="/"> */}
-            <img src={logo} className="navBar--logo"/>
-            {/* </Link> */}
+            <div className='navBar__navBarWrapper'>
 
-            <ul className="navBar--lista">
-                {/* <Link to="/ofertas"> */}
-                    <Tooltip title="OFERTAS">
-                        <li className="navBar--lista__opciones"><SavingsIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                <img src={logo} className="navBar__navBarWrapper--logo"/>
 
-                {/* <Link to="/electro"> */}
-                    <Tooltip title="ELECTRO">
-                        <li className="navBar--lista__opciones"><TvIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
+                <div className="navBar__navBarWrapper--buttonBurguer">
+                    { isOpen ? 
+                        <CloseIcon className="navBar__navBarWrapper--buttonBurguer__iconCloseMenu" onClick={toggleOpen}></CloseIcon>
+                        :
+                        <DehazeIcon className="navBar__navBarWrapper--buttonBurguer__iconOpenMenu" onClick={toggleOpen}></DehazeIcon> 
+                    }
                     
-                {/* </Link> */}
+                </div>
+                
+                <ul  className={`${!isOpen ? "navBar__navBarWrapper--lista" : "navBar__navBarWrapper--lista__verMenu"}`}> 
+                    
+                        <Tooltip title="OFERTAS">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <SavingsIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Ofertas</span> 
+                            </li>
+                        </Tooltip>
+                   
 
-                {/* <Link to="/alimentos"> */}
+                    
+                        <Tooltip title="ELECTRO">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <TvIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Electro</span> 
+                            </li>
+                        </Tooltip>
 
-                    <Tooltip title="ALIMENTOS">
-                        <li className="navBar--lista__opciones"><RestaurantIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                   
 
-                {/* <Link to="/bebidas"> */}
-                    <Tooltip title="BEBIDAS">
-                        <li className="navBar--lista__opciones"><WineBarIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                    
 
-                {/* <Link to="/limpieza"> */}
-                    <Tooltip title="LIMPIEZA">
-                        <li className="navBar--lista__opciones"><SanitizerIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                        <Tooltip title="ALIMENTOS">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <RestaurantIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Alimentos</span> 
+                            </li>
+                        </Tooltip>
+                   
 
-                {/* <Link to="/mascotas"> */}
-                    <Tooltip title="MASCOTAS">
-                        <li className="navBar--lista__opciones"><PetsIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                    
+                        <Tooltip title="BEBIDAS">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <WineBarIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Bebidas</span> 
+                            </li>
+                        </Tooltip>
+                   
 
-                {/* <Link to="/hogar"> */}
-                    <Tooltip title="HOGAR">
-                        <li className="navBar--lista__opciones"><HomeIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                    
+                        <Tooltip title="LIMPIEZA">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <SanitizerIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Limpieza</span> 
+                            </li>
+                        </Tooltip>
+                   
 
-                {/* <Link to="/automotor"> */}
-                    <Tooltip title="AUTOMOTOR">
-                        <li className="navBar--lista__opciones"><DirectionsCarIcon className='navBar--lista__opciones--icono'/></li>
-                    </Tooltip>
-                {/* </Link> */}
+                    
+                        <Tooltip title="MASCOTAS">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <PetsIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Mascotas</span> 
+                            </li>
+                        </Tooltip>
+                   
 
-                {/* <Link to="/carrito"> */}
-                    <Tooltip title="TÚ CARRITO">
-                        <li className="navBar--lista__opciones"><CardWidget/></li>
-                    </Tooltip>
-                {/* </Link> */}
-            </ul>
+                    
+                        <Tooltip title="HOGAR">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <HomeIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Hogar</span> 
+                            </li>
+                        </Tooltip>
+                   
+
+                    
+                        <Tooltip title="AUTOMOTOR">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <DirectionsCarIcon className='navBar__navBarWrapper--lista__opciones--icono'/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Automotor</span> 
+                            </li>
+                        </Tooltip>
+                   
+
+                    
+                        <Tooltip title="TÚ CARRITO">
+                            <li className="navBar__navBarWrapper--lista__opciones">
+                                <CardWidget/>
+                                <span className='navBar__navBarWrapper--lista__opciones--titulo'>Tú Carrito</span> 
+                            </li>
+                        </Tooltip>
+                   
+                </ul>
+            </div>
         </nav>
+       
+    );
 
-        <nav className="navBarMobile">
-            {/* <Link to="/"> */}
-                <img src={logo} className="navBarMobile--logo"/>
-            {/* </Link> */}
-
-            <ul className="navBarMobile--lista">
-                {/* <Link to="/ofertas"> */}
-                    <Tooltip title="OFERTAS">
-                        <li className="navBarMobile--lista__opciones"><SavingsIcon className='navBarMobile--lista__opciones--icono'/> Ofertas</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/electro"> */}
-                    <Tooltip title="ELECTRO">
-                        <li className="navBarMobile--lista__opciones"><TvIcon className='navBarMobile--lista__opciones--icono'/> Electro</li>
-                    </Tooltip>
-
-                {/* </Link> */}
-
-                {/* <Link to="/alimentos"> */}
-
-                    <Tooltip title="ALIMENTOS">
-                        <li className="navBarMobile--lista__opciones"><RestaurantIcon className='navBarMobile--lista__opciones--icono'/> Alimentos</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/bebidas"> */}
-                    <Tooltip title="BEBIDAS">
-                        <li className="navBarMobile--lista__opciones"><WineBarIcon className='navBarMobile--lista__opciones--icono'/> Bebidas</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/limpieza"> */}
-                    <Tooltip title="LIMPIEZA">
-                        <li className="navBarMobile--lista__opciones"><SanitizerIcon className='navBarMobile--lista__opciones--icono'/> Limpieza</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/mascotas"> */}
-                    <Tooltip title="MASCOTAS">
-                        <li className="navBarMobile--lista__opciones"><PetsIcon className='navBarMobile--lista__opciones--icono'/> Mascotas</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/hogar"> */}
-                    <Tooltip title="HOGAR">
-                        <li className="navBarMobile--lista__opciones"><HomeIcon className='navBarMobile--lista__opciones--icono'/> Hogar</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/automotor"> */}
-                    <Tooltip title="AUTOMOTOR">
-                        <li className="navBarMobile--lista__opciones"><DirectionsCarIcon className='navBarMobile--lista__opciones--icono'/> Automotor</li>
-                    </Tooltip>
-                {/* </Link> */}
-
-                {/* <Link to="/carrito"> */}
-                    <Tooltip title="TÚ CARRITO">
-                        <li className="navBarMobile--lista__opciones"><CardWidget/> Carrito</li>
-                    </Tooltip>
-                {/* </Link> */}
-            </ul>
-        </nav>
-        </>
-      );
 }
 
 export default NavBar;
