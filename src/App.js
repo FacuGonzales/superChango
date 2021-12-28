27d9806d115acc19ab148188e08e4c5d16a9ef2d
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-12-28 19:05:47 -0300

    Se importan en app los dos componentes nuevos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // import logo from './logo.svg';
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import "./styles/main.scss";
 import NavBar from './utils/NavBar';
 
 function App() {
+
+  const mensaje = 'Hola! Soy el primer componente';
   return (
-    <NavBar/>
+    <>
+      <NavBar/>
+
+      <ItemListContainer greeting={mensaje}/>
+    </>
   );
 }
 
