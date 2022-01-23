c85f4541f2e57a481a5e0547b56884d49e794bbf
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-23 20:19:28 -0300

    Se comenta codigo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+// import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 
-import ItemDetailContainer from './components/ItemDetailContainer';
-import ItemListContainer from "./components/ItemListContainer";
-import ItemList from './components/ItemListContainer/ItemList';
-import NavBar from "./components/NavBar";
+// import ItemDetailContainer from './components/ItemDetailContainer';
+// import ItemListContainer from "./components/ItemListContainer";
+// import ItemList from './components/ItemListContainer/ItemList';
+// import NavBar from "./components/NavBar";
 import { Navigation } from './router/Navigation';
 import "./styles/main.scss";
 
