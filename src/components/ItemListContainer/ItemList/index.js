4b93c71d196a1f658889d15b67c91b969c6fb508
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-23 19:12:20 -0300

    Se arma filtros en listados

--- src/components/ItemListContainer/ItemList/index.js
+++ src/components/ItemListContainer/ItemList/index.js
@@ -1,14 +1,28 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
 import Item from '../Item';
 import ItemData from '../../../utils/Items.json';
 
 const ItemList = () => {
     const [items, setItems] = useState ([]);
+    const {categoria} = useParams()
 
     useEffect( ()=>{
+        let db = ItemData;
+        // let listado = categoria ?  db.map(element => element.category === categoria) : db;
+        let listado = [];
+        
+        if(categoria){
+            db.map(element => {
+                if(element.category === categoria) listado.push(element);
+            });
+        }else{
+            listado = db;
+        }
+        
         const promList = new Promise ((resolve) =>{
             setTimeout (()=>{
-                resolve(ItemData);
+                resolve(listado);
             },2000);
         });
 
@@ -18,9 +32,21 @@ const ItemList = () => {
     }, []); 
     
     return (
-        <div className="itemListContainer--itemsList">
-            {items.map((item)=> <Item item={item} key={item.id} />)}
-        </div>
+        <>
+            { categoria ? 
+                <div className="itemListContainer">                    
+                    <div className="itemListContainer--itemsList">
+                        {items.map((item)=> <Item item={item} key={item.id} />)}
+                    </div> 
+                </div>
+                :
+                <div className="itemListContainer--itemsList">
+                    {items.map((item)=> <Item item={item} key={item.id} />)}
+                </div>
+            }
+        </>
+
+        
     )
 };
 
