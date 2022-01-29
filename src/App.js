84ac3d84c0a4e816c44fbb7ae2f12a62938d8a15
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-29 12:24:06 -0300

    Se importa CartProvider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import "./styles/main.scss";
+
 import React from 'react';
 import { BrowserRouter ,Route,Routes } from 'react-router-dom';
 
+
+import NavBar from "./components/NavBar";
+import CartContainer from './components/CartContainer';
+import CartProvider from './services/CartProvider';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from "./components/ItemListContainer";
-import NavBar from "./components/NavBar";
-import "./styles/main.scss";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar/>
- 
-      <Routes>
-        <Route exact path="/" element={ <ItemListContainer/> } />
-        <Route path="/category/:categoria" element={ <ItemListContainer/> } />
-        <Route path="/item/:id" element={ <ItemDetailContainer/> }  />
-      </Routes>
+      <CartProvider>
+
+        <NavBar/>
+  
+        <Routes>
+          <Route exact path="/" element={ <ItemListContainer/> } />
+          <Route path="/category/:categoria" element={ <ItemListContainer/> } />
+          <Route path="/item/:id" element={ <ItemDetailContainer/> }  />
+          <Route path="/carrito" element={ <CartContainer/> } />
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
