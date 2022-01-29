1a16a7db0a61809ad32fa669a7558ecfb085b7a6
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-29 12:04:27 -0300

    Se arma funcionalidades de ItemCount

--- src/components/ItemCount/index.js
+++ src/components/ItemCount/index.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Link } from 'react-router-dom';
+import { Button, ButtonGroup } from '@mui/material';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 function ItemCount({ stock, initial, onClick }){
     const [ count, setCount ] = useState(parseInt(initial));
@@ -13,19 +17,41 @@ function ItemCount({ stock, initial, onClick }){
         if(count > 0) setCount(count - 1);
     }
 
+    const buttons = [
+        <Link to="/">
+            <Button variant="outlined" startIcon={<KeyboardBackspaceIcon />}>
+                Volver
+            </Button>
+        </Link>,
+
+        <Button onClick={()=>onClick(count)} variant="contained" startIcon={<AddShoppingCartIcon />}>
+            Agregar
+        </Button>
+    ];
+
     return(
 
         <div className="contadorContainer">
-            <div className="contadorContainer--stockContainer">
-                <p className="contadorContainer--stockContainer__valor">Stock disponible: {stock}</p>
+            <div className="contadorContainer--infoContainer">
+
+                <div className="contadorContainer--infoContainer__stockContainer">
+                    <p className="contadorContainer--infoContainer__stockContainer--valor">Stock disponible: {stock}</p>
+                </div>
+
+                <div className="contadorContainer--infoContainer__botonAgregarRestar">
+                    <RemoveCircleIcon className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon" onClick={subsctracAmount}></RemoveCircleIcon>
+
+                    <p className="contadorContainer--infoContainer__botonAgregarRestar--valorInicial">{count}</p>
+
+                    <AddCircleIcon className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon" onClick={addAmount}></AddCircleIcon>
+                </div>
             </div>
 
-            <div className="contadorContainer--botonAgregarRestar">
-                <AddCircleIcon className="contadorContainer--botonAgregarRestar__botonIcon" onClick={addAmount}></AddCircleIcon>
 
-                <p className="contadorContainer--botonAgregarRestar__valorInicial">{count}</p>
-
-                <RemoveCircleIcon className="contadorContainer--botonAgregarRestar__botonIcon" onClick={subsctracAmount}></RemoveCircleIcon>
+            <div className="contadorContainer--botonesContainer">
+                <ButtonGroup>
+                   {buttons}
+                </ButtonGroup>
             </div>
         </div>
     )
