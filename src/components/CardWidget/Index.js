706d0fa71fb04a5c5d50a8a9d8a7018ab1629a6b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-29 12:33:52 -0300

    Se agrega badge con cantidad de items

--- src/components/CardWidget/Index.js
+++ src/components/CardWidget/Index.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Badge } from '@mui/material';
+import { CartContext } from '../Context';
 
 const CardWidget = () => {
+    const { items } = useContext(CartContext);
+    const [itemsCount, setItemsCount] = useState(0);
+
+    useEffect(() => {
+        setItemsCount(items.length);
+    }, [items])
+
 
     return(
-        <Badge className='contador' color="secondary">
+        <Badge badgeContent={itemsCount} className='contador' color="success">
             <ShoppingCartIcon className='contador__icono'/>
         </Badge>
     );
