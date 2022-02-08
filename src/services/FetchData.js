a5b8ef2ca8c095831e60665685dac023333eaa21
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-02-08 13:50:23 -0300

    Se arma peticion

--- src/services/FetchData.js
+++ src/services/FetchData.js
@@ -1,10 +1,52 @@
-import { getProducts } from '../utils/Items';
+// import { getProducts } from '../utils/Items';
+// import { useState, useEffect } from 'react';
+import { collection, getDocs, getDoc,doc, query, where} from "firebase/firestore";
+import { db } from "../utils/DataBase"
 
-export const fetchData = () => {
+const getById = async (id) => {
+    const docRef = doc(db, 'productos', id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        const item = docSnap.data();
+        const id = docSnap.id;
+
+        const prod = {...item, id};
+        return(prod)
+    }
+}
+
+const getData = async (filtro, ofertas) => {
+    let resultadoDB;
+
+    if(filtro){
+        resultadoDB = query(collection(db, "productos"), where("category", "==", filtro));
+    }else if(ofertas){
+        resultadoDB = query(collection(db, "productos"), where("oferta", "==", ofertas));
+    }
+    
+    const querySnapshot =  await getDocs(resultadoDB);
+     
+    const prod = querySnapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+    })
+
+    return(prod)
+};
+
+
+export const fetchData = (id, filtro, ofertas) => {
     try{
-        const response = getProducts;
+        let response = {};
+
+        if(id) {
+            response = getById(id)
+        }else{
+            response = getData(filtro, ofertas);
+        }
+        
         return response;
-    }catch(error){
-        console.error(error)
+    }catch(err){
+        console.error(err)
     }
 } 
